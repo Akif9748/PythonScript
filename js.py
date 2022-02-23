fa1976b396b4c513c9a5e8c42aabddca2df1596b
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-02-23 20:36:33 +0300

    Create js.py

---
+++ js.py
@@ -0,0 +1,22 @@
+from cmath import isfinite, isnan
+import os
+import math as Math
+import json
+from datetime import datetime as Date
+
+Number = float
+String = str
+isNaN = isnan
+isFinite = isfinite
+
+
+class JSON:
+    stringify = json.loads
+    parse = json.dumps
+
+
+class console:
+    debug = warn = dir = info = error = log = print
+
+    def clear():
+        os.system("cls")
